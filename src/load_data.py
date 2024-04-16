d158d0b0cbdafff4427faae979a4088777ffa3bb
vicviod <vicviodlogin@gmail.com>
2024-04-16 14:37:50 +0200

    changes

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import pyreadstat as prs
 
-def load_gss(filename, vars_to_load):
-    return pd.read_stata(filename, columns=vars_to_load)
+def load_gss(filename, vars_to_load, filetype="sav"):
+    if filetype == "sav":
+        df, meta = prs.read_sav(filename, usecols=vars_to_load)
+
+    return df, meta
+
+
 
 def load_anes(filename, vars_to_load):
     return pd.read_csv(filename, use_cols=columns_to_load)
