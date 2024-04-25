e787ba474b5c9faf0918a77a0bcdc72e9ff971a9
vicviod <vicviodlogin@gmail.com>
2024-04-25 20:22:07 +0200

    stuff

---
+++ src/corr_networks.py
@@ -0,0 +1,47 @@
+from pingouin import pairwise_corr
+
+def partial_correlations(vars, data, method):
+    """
+    for list of variables `vars` which are names of columns in the dataframe `data`, calculate the  partial correlations of
+    all pairs of variables conditioned on all other variables. use correlation type `method` (either "spearman" or "pearson")
+    """
+    corr_dfs = []
+    corr_mat = np.zeros((len(vars), len(vars)))
+
+    for i in range(len(vars)):
+        for j in range(i+1, len(vars)):
+            covar_list = vars[:i] + vars[i+1:j] + vars[j+1:]
+            corr_df = partial_corr(data=data, x=i, y=j, covar=covar_list, alternative='two-sided', method=method)
+            corr_df["x"] = i
+            corr_df["y"] = j
+            corr_mat[i, j] = corr_df.loc[method, "r"]
+            corr_mat[j, i] = corr_df.loc[method, "r"]
+            corr_dfs.append(corr_df)
+
+    corr_info = pd.concat(corr_dfs)
+    np.fill_diagonal(corr_mat, 1)
+    return corr_info, corr_mat  
+
+def gen_partial_polychor_corr_net():
+    return
+
+def lasso_reg_partial_polychor_chor_net():
+    return 
+
+def gen_partial_spearman_corr_net(df):
+    """
+    calculates the partial spearman correlation coefficients between every pair of columns in 
+    `df`. Returns that
+    """
+    pairwise_corr(df, columns=None, covar=None, alternative='two-sided', method='spearman', padjust='none', nan_policy='pairwise')
+
+Parameters
+    pairwise_corr()
+    return
+
+def gen_full_polychor_corr_net():
+    return
+
+def gen_full_spearman_corr_net():
+    return
+
