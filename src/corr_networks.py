5b3a6f1812b347455637e5c962a77f08f4be0fb1
vicviod <vicviodlogin@gmail.com>
2024-04-25 20:54:48 +0200

    give option for non partial correlations

--- src/corr_networks.py
+++ src/corr_networks.py
@@ -1,6 +1,8 @@
-from pingouin import pairwise_corr
+import numpy as np
+import pandas as pd
+from pingouin import partial_corr
 
-def partial_correlations(vars, data, method):
+def pairwise_correlations(vars, data, method, partial=True):
     """
     for list of variables `vars` which are names of columns in the dataframe `data`, calculate the  partial correlations of
     all pairs of variables conditioned on all other variables. use correlation type `method` (either "spearman" or "pearson")
@@ -10,8 +12,13 @@ def partial_correlations(vars, data, method):
 
     for i in range(len(vars)):
         for j in range(i+1, len(vars)):
-            covar_list = vars[:i] + vars[i+1:j] + vars[j+1:]
-            corr_df = partial_corr(data=data, x=i, y=j, covar=covar_list, alternative='two-sided', method=method)
+
+            if partial:
+                covar_list = vars[:i] + vars[i+1:j] + vars[j+1:]
+            else:
+                covar_list = None
+            
+            corr_df = partial_corr(data=data, x=vars[i], y=vars[j], covar=covar_list, alternative='two-sided', method=method)
             corr_df["x"] = i
             corr_df["y"] = j
             corr_mat[i, j] = corr_df.loc[method, "r"]
@@ -34,10 +41,6 @@ def gen_partial_spearman_corr_net(df):
     `df`. Returns that
     """
     pairwise_corr(df, columns=None, covar=None, alternative='two-sided', method='spearman', padjust='none', nan_policy='pairwise')
-
-Parameters
-    pairwise_corr()
-    return
 
 def gen_full_polychor_corr_net():
     return
